3e632a6945a82f53c4754860ef9981db95202bf0
Luis Perez <luis998.ap@gmail.com>
2024-03-25 13:45:07 -0500

    'Message'

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,9 +1,15 @@
 import os
 
+def printAndExecute():
+    print("Hello From a function")
+
+printAndExecute()
 
 print("add-commit-push")
 print("Executing git status: ")
 os.system("git status")
+
+
 print("Executing git add: ")
 os.system("git add -A")
 os.system("git commit -m 'Message'")
