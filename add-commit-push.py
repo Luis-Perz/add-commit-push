40d35452b31d546424f31a696068a57adad8ce3c
Luis Perez <luis998.ap@gmail.com>
2024-03-27 20:06:37 -0500

    File Updated

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,40 +1,60 @@
 import os
-def addFile(userInput): 
-    if userInput != "y":
-        exit      
-    else:
+
+# creating functions for git add, commit, and push
+
+def addFile(): 
         print("Executing git add. . . ")
         os.system("git add -A")
         os.system("git status")
 
-def commitFile(userInput):
-    if userInput != "y":
-        exit      
+def commitFile():
+    print("Executing git commit. . . ")
+    msgOption = input("Would you like to add a message?")
+    if msgOption != "y":
+        os.system('git commit -m "File Updated"')
     else:
-        print("Executing git commit. . . ")
-        msgOption = input("Would you like to add a message?")
-        if msgOption != 'y':
-            os.system("git commit -m 'File Updated'")
-        else:
-            commitMessage = userMsg()
-            os.system("git commit -m'" + commitMessage + "'")
+        commitMessage = userMsg()
+        os.system("git commit -m'" + commitMessage + "'")
 
+# creating a function to store default commit message and user generated message
 def userMsg():
     inputMsg = input("Please enter a message: \n")
     return inputMsg
 
 def pushFile(userInput):
     if userInput != "y":
-        exit
+        exit()
     else:
         print("Executing git push. . . ")
         os.system("git push")
         os.system("git status")
         
-
+# beginning of script / retrieving git status
 print("Executing git status. . . ")
 os.system("git status")
 
-addFile(input("Would you like to stage files for commit?"))
-commitFile(input("Would you like to commit changes?"))
-pushFile("Would you like to push?")
+# ask user if they would like to add file(s)
+userAdd = input("Would you like to stage files for commit?")
+if userAdd != "y":
+     print("Goodbye. . .")
+     exit()
+else:
+    addFile()
+
+# ask user if they would like to commit changes
+userCommit = input("Would you like to commit changes?")
+if userCommit != "y":
+    print("Goodbye. . .")
+    exit()
+else:
+   commitFile() 
+
+#ask user if they would like to git push
+userPush = input("Would you like to push?")
+if userPush != "y":
+    print("Goodbye. . .")
+    exit()
+else:
+   pushFile() 
+
+print("Hello world")
